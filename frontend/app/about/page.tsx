7acf521ae3f898939e49835b4320289195b1e677
codebyshoaib <shobbi800@gmail.com>
2026-02-04 09:59:36 +0500

    puts testimonial section on about us page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,12 +1,14 @@
 import dynamic from 'next/dynamic';
 import AboutBanner from "@/components/pages/about/AboutBanner";
 import AboutInfo from "@/components/pages/about/about-info";
+import TestimonialsSection from '@/components/pages/homepage/TestimonialsSection';
 
 export default async function AboutPage() {
     return (
         <main className="min-h-screen bg-white">
             <AboutBanner />
             <AboutInfo />
+            <TestimonialsSection />
         </main>
     );
 }
